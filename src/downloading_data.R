c5caec658649bc51305ab45e27a192f5c325811a
Paulus Hovens <p.p.hovens@tilburguniversity.edu>
2021-09-18 20:41:29 +0200

    - removed old folders and added new clean folders - added .gitignore since it was gone - added source code to download data from Madrid

---
+++ src/downloading_data.R
@@ -0,0 +1,3 @@
+# Download data Madrid (Biggest city to start with)
+download.file("http://data.insideairbnb.com/spain/comunidad-de-madrid/madrid/2021-07-07/data/listings.csv.gz", "data/Listings_Madrid.csv")
+download.file("http://data.insideairbnb.com/spain/comunidad-de-madrid/madrid/2021-07-07/data/reviews.csv.gz", "data/Reviews_Madrid.csv")
