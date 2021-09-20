8860aaa42f282ed1d84cb4ca590ef2e32ac53181
Doria <wentingwang@live.cn>
2021-09-20 16:01:48 +0200

    linked with google

--- src/downloading_and_cleaning_data.R
+++ src/downloading_and_cleaning_data.R
@@ -1,14 +1,53 @@
 # Download data
-dir.create(("data"), showWarnings = FALSE)
-download.file("http://data.insideairbnb.com/spain/catalonia/barcelona/2021-07-07/data/listings.csv.gz", "data/listings_barcelona.csv")
-download.file("http://data.insideairbnb.com/spain/catalonia/girona/2021-07-22/data/listings.csv.gz", "data/listings_girona.csv")
-download.file("http://data.insideairbnb.com/spain/comunidad-de-madrid/madrid/2021-07-07/data/listings.csv.gz", "data/listings_madrid.csv")
-download.file("http://data.insideairbnb.com/spain/andaluc%C3%ADa/malaga/2021-07-23/data/listings.csv.gz", "data/listings_malaga.csv")
-download.file("http://data.insideairbnb.com/spain/islas-baleares/menorca/2021-07-23/data/listings.csv.gz", "data/listings_menorca.csv")
-download.file("http://data.insideairbnb.com/spain/andaluc%C3%ADa/sevilla/2021-07-22/data/listings.csv.gz", "data/listings_sevilla.csv")
-download.file("http://data.insideairbnb.com/spain/vc/valencia/2021-07-13/data/listings.csv.gz", "data/listings_valencia.csv")
 
-# Add data into R
+## Libraries
+library(haven)
+library(googledrive)
+
+## Original Data file
+data_id <-"11MaNZ2_XatX9OGZNkJnY9mWSPsZHuu3U"
+out_file <- "data/listings_barcelona.csv"
+drive_download(as_id(data_id), 
+    path = out_file, 
+    overwrite = TRUE)
+
+data_id <-"1hSCeEx0FySiw2J8cEt6iAKDIoK-7KDxN"
+out_file <- "data/listings_girona.csv"
+drive_download(as_id(data_id), 
+               path = out_file, 
+               overwrite = TRUE)
+
+data_id <-"1ftXX8qpWq0iXcAHN4SGzcY8BMWU0UrGV"
+out_file <- "data/listings_madrid.csv"
+drive_download(as_id(data_id), 
+               path = out_file, 
+               overwrite = TRUE)
+
+data_id <-"10QZ9KuXmtIBdGUqv290vXY9ucTzrK8wR"
+out_file <- "data/listings_malaga.csv"
+drive_download(as_id(data_id), 
+               path = out_file, 
+               overwrite = TRUE)
+
+data_id <-"1K3FaAC5wBwm71IH31z7L87TCSS2HTImS"
+out_file <- "data/listings_menorca.csv"
+drive_download(as_id(data_id), 
+               path = out_file, 
+               overwrite = TRUE)
+
+data_id <-"1zQmeH6kdUdXogeqQTb8avmkOA3BOgFG3"
+out_file <- "data/listings_sevilla.csv"
+drive_download(as_id(data_id), 
+               path = out_file, 
+               overwrite = TRUE)
+
+data_id <-"1AJzjZAgHqYsVQu2P9tsC-M-zWwJoA7p-"
+out_file <- "data/listings_valencia.csv"
+drive_download(as_id(data_id), 
+               path = out_file, 
+               overwrite = TRUE)
+
+## Add data into R
 listings_barcelona <- read.csv("data/listings_barcelona.csv")
 listings_girona <- read.csv("data/listings_girona.csv")
 listings_madrid <- read.csv("data/listings_madrid.csv")
@@ -17,7 +56,7 @@ listings_menorca <- read.csv("data/listings_menorca.csv")
 listings_sevilla <- read.csv("data/listings_sevilla.csv")
 listings_valencia <- read.csv("data/listings_valencia.csv")
 
-# Add column with city
+## Add column with city
 listings_barcelona$city <- "Barcelona"
 listings_girona$city <- "Girona"
 listings_madrid$city <- "Madrid"
@@ -26,7 +65,7 @@ listings_menorca$city <- "Menorca"
 listings_sevilla$city <- "Sevilla"
 listings_valencia$city <- "Valencia"
 
-# Adding dummy variable for city size small/medium/big
+## Adding dummy variable for city size small/medium/big
 listings_barcelona$size_small <- 0
 listings_barcelona$size_medium <- 0
 listings_barcelona$size_large <- 1
@@ -49,7 +88,7 @@ listings_valencia$size_small <- 0
 listings_valencia$size_medium <- 1
 listings_valencia$size_large <- 0
 
-# Creating a data frame
+## Creating a data frame
 listings_barcelona <- listings_barcelona %>% select(18, 33, 56, 61:67, 75:78)
 listings_girona <- listings_girona %>% select(18, 33, 56, 61:67, 75:78)
 listings_madrid <- listings_madrid %>% select(18, 33, 56, 61:67, 75:78)
@@ -58,7 +97,7 @@ listings_menorca <- listings_menorca %>% select(18, 33, 56, 61:67, 75:78)
 listings_sevilla <- listings_sevilla %>% select(18, 33, 56, 61:67, 75:78)
 listings_valencia <- listings_valencia %>% select(18, 33, 56, 61:67, 75:78)
 
-# Deleting missing values
+## Deleting missing values
 listings_barcelona <- na.omit(listings_barcelona)
 listings_girona <- na.omit(listings_girona)
 listings_madrid <- na.omit(listings_madrid)
