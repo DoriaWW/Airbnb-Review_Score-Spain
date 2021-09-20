d711a53c07153dee01cd645197fc8afc10640804
Paulus Hovens <p.p.hovens@tilburguniversity.edu>
2021-09-20 19:23:09 +0200

    removed size_large

--- src/downloading_and_cleaning_data.R
+++ src/downloading_and_cleaning_data.R
@@ -1,15 +1,17 @@
-# Download data
-
 ## Libraries
+install.packages(haven)
+install.packages(googledrive)
 library(haven)
 library(googledrive)
+library(tidyverse)
 
 ## Original Data file
+dir.create(("data"), showWarnings = FALSE)
 data_id <-"11MaNZ2_XatX9OGZNkJnY9mWSPsZHuu3U"
 out_file <- "data/listings_barcelona.csv"
 drive_download(as_id(data_id), 
-    path = out_file, 
-    overwrite = TRUE)
+               path = out_file, 
+               overwrite = TRUE)
 
 data_id <-"1hSCeEx0FySiw2J8cEt6iAKDIoK-7KDxN"
 out_file <- "data/listings_girona.csv"
@@ -46,6 +48,7 @@ out_file <- "data/listings_valencia.csv"
 drive_download(as_id(data_id), 
                path = out_file, 
                overwrite = TRUE)
+rm(data_id, out_file)
 
 ## Add data into R
 listings_barcelona <- read.csv("data/listings_barcelona.csv")
@@ -68,34 +71,27 @@ listings_valencia$city <- "Valencia"
 ## Adding dummy variable for city size small/medium/big
 listings_barcelona$size_small <- 0
 listings_barcelona$size_medium <- 0
-listings_barcelona$size_large <- 1
 listings_girona$size_small <- 1
 listings_girona$size_medium <- 0
-listings_girona$size_large <- 0
 listings_madrid$size_small <- 0
 listings_madrid$size_medium <- 0
-listings_madrid$size_large <- 1
 listings_malaga$size_small <- 0
 listings_malaga$size_medium <- 1
-listings_malaga$size_large <- 0
 listings_menorca$size_small <- 1
 listings_menorca$size_medium <- 0
-listings_menorca$size_large <- 0
 listings_sevilla$size_small <- 0
 listings_sevilla$size_medium <- 1
-listings_sevilla$size_large <- 0
 listings_valencia$size_small <- 0
 listings_valencia$size_medium <- 1
-listings_valencia$size_large <- 0
 
 ## Creating a data frame
-listings_barcelona <- listings_barcelona %>% select(18, 33, 56, 61:67, 75:78)
-listings_girona <- listings_girona %>% select(18, 33, 56, 61:67, 75:78)
-listings_madrid <- listings_madrid %>% select(18, 33, 56, 61:67, 75:78)
-listings_malaga <- listings_malaga %>% select(18, 33, 56, 61:67, 75:78)
-listings_menorca <- listings_menorca %>% select(18, 33, 56, 61:67, 75:78)
-listings_sevilla <- listings_sevilla %>% select(18, 33, 56, 61:67, 75:78)
-listings_valencia <- listings_valencia %>% select(18, 33, 56, 61:67, 75:78)
+listings_barcelona <- listings_barcelona %>% select(18, 33, 56, 61:67, 75:77)
+listings_girona <- listings_girona %>% select(18, 33, 56, 61:67, 75:77)
+listings_madrid <- listings_madrid %>% select(18, 33, 56, 61:67, 75:77)
+listings_malaga <- listings_malaga %>% select(18, 33, 56, 61:67, 75:77)
+listings_menorca <- listings_menorca %>% select(18, 33, 56, 61:67, 75:77)
+listings_sevilla <- listings_sevilla %>% select(18, 33, 56, 61:67, 75:77)
+listings_valencia <- listings_valencia %>% select(18, 33, 56, 61:67, 75:77)
 
 ## Deleting missing values
 listings_barcelona <- na.omit(listings_barcelona)
