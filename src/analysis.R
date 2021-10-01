e62e75adaa5d544429ca18d9f175d8a00e915d03
RalphGit21 <r.r.delsing@tilburguniversity.edu>
2021-10-01 22:45:07 +0200

    Created 3 new descriptive tables, improved code

--- src/analysis.R
+++ src/analysis.R
@@ -21,33 +21,57 @@ table_descriptives1 <- listings_all %>%
             number_shared_rooms = sum((room_type_entire == 0) & (room_type_hotel ==0) & (room_type_private ==0)),
             number_of_superhosts = sum(as.numeric(host_is_superhost)))
 
+
 table_descriptives2 <- listings_all %>%
   group_by(size) %>%
   count()
 
-table_descriptives <- table_descriptives1 %>%
+table_descriptives3 <- table_descriptives1 %>%
   inner_join(table_descriptives2, by = "size")
 
-table_descriptives <- table_descriptives %>%
-  mutate(ratio_superhosts = number_of_superhosts / n, table_descriptives) %>%
-  mutate(across(where(is.numeric), ~ round(., digits = 2)))  
+table_descriptives3 <- table_descriptives3 %>%
+  mutate(ratio_superhosts = number_of_superhosts / n, table_descriptives3) %>%
+  mutate(across(where(is.numeric), ~ round(., digits = 3)))  
 
 
-y <- rbind(table_descriptives, c("Total", 
+table_descriptives <- rbind(table_descriptives3, c("Total", 
                                  colMeans(listings_all[,c("review_scores_rating","review_scores_location", "review_scores_value", "number_of_reviews", "review_scores_cleanliness", "review_scores_checkin", "review_scores_communication", "review_scores_accuracy")]), 
                                  colSums(listings_all[,c("room_type_private", "room_type_hotel", "room_type_entire")]), nrow(listings_all)-sum(listings_all[,c("room_type_private", "room_type_hotel", "room_type_entire")]),
                                  sum(listings_all[,"host_is_superhost"]), nrow(listings_all), sum(listings_all[,"host_is_superhost"])/nrow(listings_all))) %>%
   mutate_at(vars(mean_rating, mean_location, mean_value, mean_number_reviews, mean_cleanliness, mean_checkin, mean_communication, mean_accuracy, number_private_rooms, number_hotel_rooms, number_entire_rooms, number_shared_rooms, number_of_superhosts, n, ratio_superhosts), as.numeric) %>%
-  mutate(across(where(is.numeric), ~ round(., digits = 2)))  
+  mutate(across(where(is.numeric), ~ round(., digits = 3))) %>% mutate_each(funs(round(.,0)), mean_number_reviews) 
+
+
+superhost_descriptives <- listings_all %>%
+  group_by(host_is_superhost) %>%
+  summarize(mean_number_reviews = mean(number_of_reviews), mean_rating = mean(review_scores_rating)) %>% 
+  mutate_each(funs(round(.,0)), mean_number_reviews) %>% mutate_each(funs(round(.,2)), mean_rating)
+
+#This table shows that listings owned by a "superhost" (which is the case when host_is_superhost = 1) receive more than double the number of reviews than listings that are not owned by a "superhost". Moreover, the overall rating is quite higher for this group.
+
+room_type_descriptives <- listings_all %>%
+  group_by(room_type) %>%
+  summarize(mean_number_reviews = mean(number_of_reviews), mean_rating = mean(review_scores_rating)) %>%
+  mutate_each(funs(round(.,0)), mean_number_reviews) %>% mutate_each(funs(round(.,2)), mean_rating) %>%
+  arrange(desc(mean_number_reviews, mean_rating))
+
+#This table shows that room types "Entire home" and "private room" get almost double the amount of reviews compared to "shared room" and "hotel room". No significant difference in terms of ratings can be seen between these room types, although shared rooms stands out a little bit in last place.
+
+size_descriptives <- listings_all %>%
+  group_by(size) %>%
+  summarize(mean_number_reviews = mean(number_of_reviews), mean_rating = mean(review_scores_rating)) %>%
+  mutate_each(funs(round(.,0)), mean_number_reviews) %>% mutate_each(funs(round(.,2)), mean_rating) %>%
+  arrange(desc(mean_number_reviews, mean_rating)) %>% arrange(size)
+
+#In this table, it can be seen that the medium and large cities get way more reviews than smaller-sized cities. In terms of the overall rating, the three sizes score about the same. 
 
 
 
 
-listings %>%  
-  ggplot(aes(x = city,y = mean(review_scores_rating))) +
-  geom_bar(position="dodge", stat="identity") +
-  facet_wrap(~ size) +
-  theme_bw() +
-  ylab("Socre") +
+
+listings_all %>%  
+  ggplot(aes(x = number_of_reviews, y = review_scores_rating)) +
+  geom_point()
+  ylab("Score") +
   xlab("Cities") +
   ggtitle("Overview of rating scores")
